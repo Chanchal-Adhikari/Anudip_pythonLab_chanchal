1e7c0726596d701496e050e2feb3a6a5a8bcb9a3
Chanchal Adhikari <chanchaladhikari21@gmail.com>
2024-12-23 09:51:20 +0530

    Add files via upload

---
+++ lab2_part2.py
@@ -0,0 +1,14 @@
+""" Question:
+Write a program to input the temperature in Celsius
+and calculate and print it in Fahrenheit. """
+
+
+
+import math
+
+# take celcius degree from user
+celcius = int(input("Enter the Celcius temperature: "))
+
+# convert it to fahrenheit
+fahrenheit = (celcius * 9/5) + 32
+print("The temperature in Fahrenheit is ", fahrenheit)
